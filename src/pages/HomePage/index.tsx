117207db87dd8cde1179d6f9b3c54b41c05c4a1a
Chinazor Nnadozie <nnadoziechinazor@gmail.com>
2022-08-25 19:37:32 +0100

    set up

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const HomePage = () => {
+    return (
+        <Wrapper>
+            <p>Home page</p>
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.section``;
